8d91af110a94c516ced0fc74fb6eed1e7cce01c5
Maelle Berthet <berthet.maelle@hotmail.fr>
2022-05-20 16:30:28 +0200

    CPP ex02

--- CPP01/ex02/main.cpp
+++ CPP01/ex02/main.cpp
@@ -5,22 +5,27 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mberthet <mberthet@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/20 12:16:46 by mberthet          #+#    #+#             */
-/*   Updated: 2022/05/20 15:31:18 by mberthet         ###   ########.fr       */
+/*   Created: 2022/05/20 15:32:24 by mberthet          #+#    #+#             */
+/*   Updated: 2022/05/20 16:16:38 by mberthet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Zombie.hpp"
+#include <iostream>
+#include <string>
 
 int main()
 {
-	Zombie* firstZ;
-	int N = 5;
+	std::string str = "HI THIS IS BRAIN";
+	std::string* stringPTR = &str;
+	std::string& stringREF = str; //reference to the string ?
 
-	firstZ = zombieHorde(N, "Lulu");
-	for(int i = 0; i < N; i++)
-		firstZ->announce();
-	delete [] firstZ;
-	//system("leaks MoarBrainz");
+	std::cout << "Str address : " << &str << std::endl;
+	std::cout << "stringPTR address : " << stringPTR << std::endl;
+	std::cout << "stringREF address : " << &stringREF << std::endl;
+
+	std::cout << "Str : " << str << std::endl;
+	std::cout << "stringPTR : " << *stringPTR << std::endl;
+	std::cout << "stringREF : " << stringREF << std::endl;
+
 	return (0);
 }
