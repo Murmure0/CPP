0832c0c13f2e03fea01fe245020437fab0856e34
Maelle Berthet <berthet.maelle@hotmail.fr>
2022-06-07 14:17:08 +0200

    re

--- CPP03/ex01/ScavTrap.cpp
+++ CPP03/ex01/ScavTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: mberthet <mberthet@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 11:24:54 by mberthet          #+#    #+#             */
-/*   Updated: 2022/06/07 13:46:31 by mberthet         ###   ########.fr       */
+/*   Updated: 2022/06/07 14:16:48 by mberthet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,18 @@ ScavTrap::~ScavTrap( void )
 	std::cout << "Default ScavTrap destructor." << std::endl;
 }
 
-ScavTrap::ScavTrap( ScavTrap const& src )
+ScavTrap::ScavTrap( ScavTrap const& src ) : ClapTrap(src)
 {
 	std::cout << "ScavTrap Copy constructor." << std::endl;
-	*this = src;
 }
 
+void	ScavTrap::attack(const std::string &target)
+{
+	if (this->_HitPts == 0 || this->_EnergyPts == 0)
+		std::cout << "ScavTrap " << this->_name << " can't do anything, boo." << std::endl;
+	else
+	{
+		std::cout << "ScavTrap " << this->_name << " brutally attacks " << target << ", causing " << this->_AttackDmg << " dmg!" << std::endl;
+		this->_EnergyPts--;
+	}
+}
